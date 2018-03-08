38a5bfe6fcef5eb72f1b962d7a1c9e326d290ca9
Phani kashyap <phaneendra.velide@pervacio.com>
2018-03-09 00:00:34 +0530

    "commit2"

--- src/main/java/MavenSample/Jenkins/MavenSample/Jenkins/Sample.java
+++ src/main/java/MavenSample/Jenkins/MavenSample/Jenkins/Sample.java
@@ -1,10 +1,18 @@
 package MavenSample.Jenkins.MavenSample.Jenkins;
 
-public class Sample {
+import java.util.logging.Logger;
 
+import org.apache.log4j.BasicConfigurator;
+
+public class Sample {
 	public static void main(String[] args) {
+		Logger logger = Logger.getLogger("");
+		BasicConfigurator.configure();
+		logger.info("This is my first log4j's statement");
+		
 		// TODO Auto-generated method stub
 		System.out.println("Heyy!!Hello");
+		
 	}
 
 }
