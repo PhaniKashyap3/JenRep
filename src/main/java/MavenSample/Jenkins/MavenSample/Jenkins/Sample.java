1ab24a2ab0de7b288f59c34d5726f36622003a29
Phani kashyap <phaneendra.velide@pervacio.com>
2018-03-09 00:37:03 +0530

    ""

--- src/main/java/MavenSample/Jenkins/MavenSample/Jenkins/Sample.java
+++ src/main/java/MavenSample/Jenkins/MavenSample/Jenkins/Sample.java
@@ -7,11 +7,11 @@ import org.apache.log4j.BasicConfigurator;
 public class Sample {
 	public static void main(String[] args) {
 
-		Logger logger = Logger.getLogger("II");
+		/*Logger logger = Logger.getLogger("II");
 		BasicConfigurator.configure();
 		logger.info("This is my first log4j's statement");
 		logger.info("This is my first log4j's statement");
-
+*/
 		// TODO Auto-generated method stub
 		System.out.println("Heyy!!Hellooo");
 		
