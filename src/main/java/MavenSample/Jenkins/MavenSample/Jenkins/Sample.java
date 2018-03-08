bcbd57b89cee60999ac5e4df4d4f324ae8d8a74c
Phani kashyap <phaneendra.velide@pervacio.com>
2018-03-09 00:09:10 +0530

    "Reverted"

--- src/main/java/MavenSample/Jenkins/MavenSample/Jenkins/Sample.java
+++ src/main/java/MavenSample/Jenkins/MavenSample/Jenkins/Sample.java
@@ -2,15 +2,21 @@ package MavenSample.Jenkins.MavenSample.Jenkins;
 
 import java.util.logging.Logger;
 
-import org.apache.log4j.BasicConfigurator;
+//import org.apache.log4j.BasicConfigurator;
 
 public class Sample {
 	public static void main(String[] args) {
-		Logger logger = Logger.getLogger("");
+/*
+<dependency>
+	<groupId>log4j</groupId>
+	<artifactId>log4j</artifactId>
+	<version>1.2.17</version>
+</dependency>
+		Logger logger = Logger.getLogger("II");
 		BasicConfigurator.configure();
 		logger.info("This is my first log4j's statement");
 		logger.info("This is my first log4j's statement");
-
+*/
 		// TODO Auto-generated method stub
 		System.out.println("Heyy!!Hello");
 		
