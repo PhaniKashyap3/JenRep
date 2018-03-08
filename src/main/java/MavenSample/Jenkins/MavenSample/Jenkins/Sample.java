748c03b1d8c020329db888304d64e81f323106d0
Phani kashyap <phaneendra.velide@pervacio.com>
2018-03-08 23:00:16 +0530

    First commit

---
+++ src/main/java/MavenSample/Jenkins/MavenSample/Jenkins/Sample.java
@@ -0,0 +1,10 @@
+package MavenSample.Jenkins.MavenSample.Jenkins;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Heyy!!Hello");
+	}
+
+}
