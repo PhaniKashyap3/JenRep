875677a20ae19b6b0741e88865ba0f5d309677e7
Phani kashyap <phaneendra.velide@pervacio.com>
2018-03-19 15:24:52 +0530

    POM changes

--- src/main/java/MavenSample/Jenkins/MavenSample/Jenkins/Sample.java
+++ src/main/java/MavenSample/Jenkins/MavenSample/Jenkins/Sample.java
@@ -4,6 +4,7 @@ import java.util.logging.Logger;
 
 import org.apache.log4j.BasicConfigurator;
 
+
 public class Sample {
 	public static void main(String[] args) {
 
@@ -14,7 +15,14 @@ public class Sample {
 */
 		// TODO Auto-generated method stub
 		System.out.println("Heyy!!Hellooo");
-		
+		String line="Reader line .. testStatus :EventData=testLastRestart:604801";
+		String x=line.substring(line.lastIndexOf(":")+1);
+		System.out.println(x);
+			if(Integer.parseInt(x)>604800)
+System.out.println("true");			else
+System.out.println("False");		
+
+        
 	}
 
 }
